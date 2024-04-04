a8bd615d4be0240b320111c1de91b5a716ce7d25
GandhamAjay747 <ajay.gandham2@gmail.com>
2024-04-04 10:04:25 +0530

    changed again

--- src/main/java/com/ngsprogram/myprogram/MyprogramController.java
+++ src/main/java/com/ngsprogram/myprogram/MyprogramController.java
@@ -14,6 +14,6 @@ public class MyprogramController {
     @RequestMapping("/hello")
     public String hello()
     {
-        return "Hello Springg Boott..you changedd!!!";
+        return "Hello Springg Boott..you changedd againnnn!!!";
     }
 }
