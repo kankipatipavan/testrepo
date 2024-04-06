9ad7fb3aae5e8041b25e712ca8bc65760314933f
GandhamAjay747 <111268936+GandhamAjay747@users.noreply.github.com>
2024-04-06 22:29:52 +0530

    checking for final commit

--- src/main/java/com/ngsprogram/myprogram/MyprogramController.java
+++ src/main/java/com/ngsprogram/myprogram/MyprogramController.java
@@ -14,6 +14,6 @@ public class MyprogramController {
     @RequestMapping("/hello")
     public String hello()
     {
-        return "my pipeline is working fine!!!";
+        return "Idhe Final ankuntunna!!!";
     }
 }
