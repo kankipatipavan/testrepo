2eecd152d8dce056c3cbf9dcbb886c188f24ee79
GandhamAjay747 <111268936+GandhamAjay747@users.noreply.github.com>
2024-04-18 18:41:25 +0530

    chdvh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -8,5 +8,9 @@ echo "Starting the Spring Boot application..."
 # Change to the directory where your JAR file is located
 #cd /home/ec2-user/my-app/
 
+#setting permissions
+chmod +x /home/ec2-user/my-app/myprogram-0.0.1-SNAPSHOT.jar
+
 # Run the JAR file
-java -jar /home/ec2-user/my-app/myprogram-0.0.1-SNAPSHOT.jar > /home/ec2-user/my-app/app.log 2>&1 &
+java -jar /home/ec2-user/my-app/myprogram-0.0.1-SNAPSHOT.jar 
+#> /home/ec2-user/my-app/app.log 2>&1 &
