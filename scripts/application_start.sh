4a3e0618dc8e41bd1ad8ce2dc39e8083f7b66c17
GandhamAjay747 <111268936+GandhamAjay747@users.noreply.github.com>
2024-04-18 18:22:18 +0530

    star

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,7 +6,7 @@ echo "Starting the Spring Boot application..."
 #JAVA_HOME=/opt/jdk-18/bin/java
 
 # Change to the directory where your JAR file is located
-cd /home/ec2-user/my-app/
+#cd /home/ec2-user/my-app/
 
 # Run the JAR file
-java -jar myprogram-0.0.1-SNAPSHOT.jar
+java -jar /home/ec2-user/my-app/myprogram-0.0.1-SNAPSHOT.jar > /home/ec2-user/my-app/app.log 2>&1 &
