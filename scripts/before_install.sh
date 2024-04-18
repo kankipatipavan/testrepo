7886984a35008d5f8d5c87ab2a3b514384887818
GandhamAjay747 <111268936+GandhamAjay747@users.noreply.github.com>
2024-04-18 16:22:20 +0530

    java jdk 18

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,34 +5,16 @@ echo "cleaning the artifacts."
 
 #!/bin/bash
 
-# Define the Java version
-JAVA_VERSION="17"
-
-# Define the download URL for the JDK tar.gz file
-DOWNLOAD_URL="https://download.java.net/java/GA/jdk${JAVA_VERSION}/d9d758a248514b3ca22bb749fdebcb8e/35/GPL/openjdk-${JAVA_VERSION}_linux-x64_bin.tar.gz"
-
-# Define the installation directory
-INSTALL_DIR="/opt/java/jdk${JAVA_VERSION}"
-
-# Create the installation directory if it doesn't exist
-sudo mkdir -p $INSTALL_DIR
-
-# Download the JDK tar.gz file
-echo "Downloading Java JDK ${JAVA_VERSION}..."
-sudo curl -L -o /tmp/openjdk-${JAVA_VERSION}_linux-x64_bin.tar.gz $DOWNLOAD_URL
-
-# Extract the JDK tar.gz file to the installation directory
-echo "Extracting Java JDK ${JAVA_VERSION}..."
-sudo tar -xzvf /tmp/openjdk-${JAVA_VERSION}_linux-x64_bin.tar.gz -C $INSTALL_DIR --strip-components=1
-
-# Update alternatives to set Java version
-echo "Setting Java JDK ${JAVA_VERSION} as default..."
-sudo alternatives --install /usr/bin/java java $INSTALL_DIR/bin/java 1
-sudo alternatives --install /usr/bin/javac javac $INSTALL_DIR/bin/javac 1
-
-# Clean up downloaded tar.gz file
-echo "Cleaning up..."
-sudo rm -f /tmp/openjdk-${JAVA_VERSION}_linux-x64_bin.tar.gz
-
-echo "Java JDK ${JAVA_VERSION} has been successfully installed."
+sudo yum update
+sudo yum install -y curl wget
+curl -O https://download.java.net/java/GA/jdk18/43f95e8614114aeaa8e8a5fcf20a682d/36/GPL/openjdk-18_linux-x64_bin.tar.gz
+tar xvf openjdk-18_linux-x64_bin.tar.gz
+sudo mv jdk-18 /opt/
+sudo tee /etc/profile.d/jdk18.sh <<EOF
+export JAVA_HOME=/opt/jdk-18
+export PATH=\$PATH:\$JAVA_HOME/bin
+EOF
+source /etc/profile.d/jdk18.sh
+echo $JAVA_HOME
+java -version
 
