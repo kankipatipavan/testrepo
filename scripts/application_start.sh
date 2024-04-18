a888543a177850749bfbbb34b63b9d2ec79b24ab
GandhamAjay747 <111268936+GandhamAjay747@users.noreply.github.com>
2024-04-18 18:46:21 +0530

    kkkk

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,5 +12,5 @@ echo "Starting the Spring Boot application..."
 chmod +x /home/ec2-user/my-app/myprogram-0.0.1-SNAPSHOT.jar
 
 # Run the JAR file
-java -jar /home/ec2-user/my-app/myprogram-0.0.1-SNAPSHOT.jar 
+java -jar /home/ec2-user/my-app/myprogram-0.0.1-SNAPSHOT.jar > /home/ec2-user/my-app/app.log 2>&1 &
 #> /home/ec2-user/my-app/app.log 2>&1 &
