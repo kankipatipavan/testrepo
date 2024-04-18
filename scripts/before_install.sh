a8e1a967844006ed1a919b04f2ccd08782665171
GandhamAjay747 <111268936+GandhamAjay747@users.noreply.github.com>
2024-04-18 15:28:02 +0530

    no jdk

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,7 +3,3 @@
 echo "cleaning the artifacts."
 #mvn clean
 
-sudo yum install java-17-amazon-corretto-headless
-sudo yum install java-17-amazon-corretto
-sudo yum install java-17-amazon-corretto-devel
-sudo yum install java-17-amazon-corretto-jmods
