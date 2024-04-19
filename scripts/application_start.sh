d29a03e154216098643bb8b928c60916d05b1c4c
kankipati Pavan kumar Varma <74644784+kankipatipavan@users.noreply.github.com>
2024-04-19 15:51:47 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,14 +3,20 @@
 echo "Starting the Spring Boot application..."
 
 # Full path to the Java executable
-JAVA_HOME=/opt/jdk-18/bin/java
+#JAVA_HOME=/opt/jdk-18/bin/java
+
+
+export JAVA_HOME=/opt/jdk-18
+echo $JAVA_HOME
+echo $PATH
+echo "JAVA_HOME path set"
 
 # Change to the directory where your JAR file is located
 #cd /home/ec2-user/my-app/
 
 #setting permissions
-chmod u+x /home/ec2-user/my-app/myprogram-0.0.1-SNAPSHOT.jar
+chmod 777 /home/ec2-user/my-app/myprogram-0.0.1-SNAPSHOT.jar
 
 # Run the JAR file
-java -jar /home/ec2-user/my-app/myprogram-0.0.1-SNAPSHOT.jar
+java -jar /home/ec2-user/my-app/myprogram-0.0.1-SNAPSHOT.jar > /home/ec2-user/my-app/app.log 2>&1 & b
 # > /home/ec2-user/my-app/app.log 2>&1 &
